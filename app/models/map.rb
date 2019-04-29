8b1f57e366acec8b46a18eb5922c083432e2b0d6
Eduardo Straub <eduardo.straub1@gmail.com>
2019-04-29 18:43:19 -0400

    latitud y longitud se agregan automaticamente si estan vacios.

--- app/models/map.rb
+++ app/models/map.rb
@@ -13,6 +13,7 @@ class Map < ApplicationRecord
 
   def generate_zoom
     if self.zoom.nil?
+      self.zoom = 18
       '&zoom=18'
     else
       '&zoom=' + self.zoom.to_s
@@ -29,6 +30,8 @@ class Map < ApplicationRecord
     if !self.width.nil? && !self.height.nil?
       '&size=' + self.width.to_s + 'x' + self.height.to_s
     else
+      self.width = 800
+      self.height = 800
       '&size=800x800'
     end
   end
