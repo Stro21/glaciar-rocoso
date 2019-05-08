def87ac239e30daf8eb8a7e6861e3aa0f758548c
Eduardo Straub <eduardo.straub1@gmail.com>
2019-05-08 12:54:59 -0400

    Se agrega el navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :maps
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'maps#index'
+  root to: 'maplists#index'
 end
