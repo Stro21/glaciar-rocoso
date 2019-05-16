22b82e224598b447c8c94b9635ea689754de6303
Eduardo Straub <eduardo.straub1@gmail.com>
2019-05-15 20:07:31 -0400

    Se agrega el campo name a la tabla map.

--- app/views/maps/_map.json.jbuilder
+++ app/views/maps/_map.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! map, :id, :latitude, :longitude, :url, :elevation, :zoom, :width, :height, :created_at, :updated_at
+json.extract! map, :id, :name, :latitude, :longitude, :url, :elevation, :zoom, :size, :created_at, :updated_at
 json.url map_url(map, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_125924) do
+ActiveRecord::Schema.define(version: 2019_05_15_235528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_10_125924) do
     t.bigint "user_id"
     t.boolean "glaciar_rock"
     t.string "size"
+    t.string "name"
     t.index ["user_id"], name: "index_maps_on_user_id"
   end
 
