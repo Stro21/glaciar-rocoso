9239b345d3e3e55fc819027231e057bd2208c404
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-05 12:23:43 -0400

    avences con la configuración del login facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :maps
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'maps#index'
   get '/my_maps', to: 'maps#my_maps'
