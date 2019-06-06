40b5111c6e08d3206d8dd3c74708c76e4a3fcb00
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-06 16:27:31 -0400

    Se agrego la respuesta del profe.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module GlaciarRocoso
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://glaciar-rocoso.herokuapp.com/',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
