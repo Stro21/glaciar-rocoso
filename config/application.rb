9cfba1f3f987107aafeccccad3834422e5f2955c
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-06 16:41:03 -0400

    Se esta probando

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module GlaciarRocoso
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'https://glaciar-rocoso.herokuapp.com/',
+      'Access-Control-Allow-Origin' => 'https://www.facebook.com/',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
   end
