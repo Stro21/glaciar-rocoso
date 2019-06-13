7743a93ca25162d9da0db75ef7bc7d56f78d003d
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-13 15:38:20 -0400

    pequeño fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,7 @@ User.destroy_all if Rails.env.development?
 AdminUser.destroy_all
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 AdminUser.create!(email: 'admin@rails.com', password: '6Lgtt4Pvh9yYuss', password_confirmation: '6Lgtt4Pvh9yYuss') if Rails.env.production?
-admin = User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', 
-  username: 'admin', role: :admin) if Rails.env.development?
+admin = User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', username: 'admin', role: :admin) if Rails.env.development?
 User.create!(email: 'usuario@example.com', password: '123456', password_confirmation: '123456', username: 'usuario')
 Map.create!(center: '(-34.1752828573063, -69.7947257582627)', glaciar_rock: true, user: admin)
 Map.create!(center: '(-33.58385020722072, -70.14348598109588)', glaciar_rock: false, user: admin)
