2f0ad3eb082f7a43969247cf3cf22de2f4a7ad32
Eduardo Straub <eduardo.straub1@gmail.com>
2019-06-12 21:50:29 -0400

    Se deberían ir los errores de active admin

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,6 @@
 //= require active_admin/base
+document.addEventListener('turbolinks:load', function(){
+  if(document.body.classList.contains('active_admin')) {
+   //= require active_admin/base
+  }
+})
